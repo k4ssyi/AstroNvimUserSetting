5756c602812c3a5a4cf43849272366763a62b796
k4ssyi <kasikasi00@gmail.com>
2023-10-02 19:10:47 +0900

    add: git diffを使いやすく&mapping追加

--- plugins/community.lua
+++ plugins/community.lua
@@ -9,5 +9,6 @@ return {
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.scrolling.nvim-scrollbar" },
   { import = "astrocommunity.git.neogit" },
+  { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.search.nvim-hlslens" },
 }
